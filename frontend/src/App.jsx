dcaae646f81972c1dda4ba404004e5436fb50dbe
webeato <webeato.ad@gmail.com>
2026-05-10 23:01:40 +0330

    create footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -222,6 +222,30 @@ function App() {
           </div>
         </section>
       </main>
+      <footer>
+        <div className="container">
+          <ul className="footer__links">
+            <li className="footer__link">
+              <a href="#hero">خانه</a>
+            </li>
+            <li className="footer__link">
+              <a href="#about">درباره</a>
+            </li>
+            <li className="footer__link">
+              <a href="#plans">اشتراک‌ها</a>
+            </li>
+          </ul>
+          <div className="footer__info">
+            <p className="footer__copyright">
+              بـیـتـوجـیـم. تمامی حقوق محفوظ است.
+            </p>
+            <p className="footer__developers">
+              Built by <a href="https://github.com/WeBeato">Webeato</a> and{" "}
+              <a href="https://github.com/Its-ech">Its-ech</a>.
+            </p>
+          </div>
+        </div>
+      </footer>
     </>
   );
 }
